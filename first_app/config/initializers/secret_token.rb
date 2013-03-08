0090789ec80a03fea94348e97c165fb33b85caa4
idlexp <idlexp@yahoo.co.uk>
2013-03-08 21:04:41 +0000

    initial commit

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f7349082e8626354c4df5735ef1a7a25b2eee54c9afe8528a8bf6db3185e028812f8a31bf1d96b0a98668f0c7269ddf9c5ae1c77a84786bf5ceb5d332ec7ba1e'
